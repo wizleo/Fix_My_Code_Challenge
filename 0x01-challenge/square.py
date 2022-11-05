662b148917f7e4d0ab07e9f7960fbaae628abf60
wizleo <nkajjasolomon420@gmail.com>
2022-11-05 07:01:38 +0000

    0x01-challenge tasks

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+module for Square clas
+"""
+
+
+class Square:
+    """ Square class """
+
+    def __init__(self, *args, **kwargs):
+        """ initiailize a new Square obj """
+        self.width = 0
+        self.height = 0
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """ Perimeter of the square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ formatted attributes """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
+
+
+
